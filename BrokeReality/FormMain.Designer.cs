f60b85e41a855b3fb8a9aad43a24a39d8d2f6c65
Razvan Badea <rzvbadea@gmail.com>
2011-09-22 10:25:48 +0300

    basic functionalities

---
+++ BrokeReality/FormMain.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace BrokeReality
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxCaptcha = new System.Windows.Forms.PictureBox();
+            this.labelFile = new System.Windows.Forms.Label();
+            this.openFileDialogImage = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxImage = new System.Windows.Forms.TextBox();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.buttonSplit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCaptcha)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxCaptcha
+            // 
+            this.pictureBoxCaptcha.Location = new System.Drawing.Point(13, 186);
+            this.pictureBoxCaptcha.Name = "pictureBoxCaptcha";
+            this.pictureBoxCaptcha.Size = new System.Drawing.Size(129, 63);
+            this.pictureBoxCaptcha.TabIndex = 0;
+            this.pictureBoxCaptcha.TabStop = false;
+            // 
+            // labelFile
+            // 
+            this.labelFile.AutoSize = true;
+            this.labelFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFile.Location = new System.Drawing.Point(13, 13);
+            this.labelFile.Name = "labelFile";
+            this.labelFile.Size = new System.Drawing.Size(109, 17);
+            this.labelFile.TabIndex = 1;
+            this.labelFile.Text = "Select an image";
+            // 
+            // openFileDialogImage
+            // 
+            this.openFileDialogImage.Filter = "Image file *bmp|*bmp";
+            // 
+            // textBoxImage
+            // 
+            this.textBoxImage.Location = new System.Drawing.Point(13, 34);
+            this.textBoxImage.Name = "textBoxImage";
+            this.textBoxImage.Size = new System.Drawing.Size(212, 20);
+            this.textBoxImage.TabIndex = 2;
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(232, 32);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(79, 23);
+            this.buttonSelect.TabIndex = 3;
+            this.buttonSelect.Text = "Select";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Location = new System.Drawing.Point(13, 85);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(75, 23);
+            this.buttonProcess.TabIndex = 4;
+            this.buttonProcess.Text = "Process";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
+            // 
+            // buttonSplit
+            // 
+            this.buttonSplit.Enabled = false;
+            this.buttonSplit.Location = new System.Drawing.Point(95, 84);
+            this.buttonSplit.Name = "buttonSplit";
+            this.buttonSplit.Size = new System.Drawing.Size(75, 23);
+            this.buttonSplit.TabIndex = 5;
+            this.buttonSplit.Text = "Split";
+            this.buttonSplit.UseVisualStyleBackColor = true;
+            this.buttonSplit.Click += new System.EventHandler(this.buttonSplit_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 261);
+            this.Controls.Add(this.buttonSplit);
+            this.Controls.Add(this.buttonProcess);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.textBoxImage);
+            this.Controls.Add(this.labelFile);
+            this.Controls.Add(this.pictureBoxCaptcha);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BrokenReality";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCaptcha)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxCaptcha;
+        private System.Windows.Forms.Label labelFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialogImage;
+        private System.Windows.Forms.TextBox textBoxImage;
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.Button buttonSplit;
+    }
+}
+
---
+++ BrokeReality/FormMain.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BrokeReality
+{
+    public partial class FormMain : Form
+    {
+        #region Constants
+        private Color LIGHTESTCOLOR = Color.FromArgb(255, 236, 236, 236);
+        private Color GRIDCOLOR = Color.FromArgb(255, 204, 204, 204);
+        #endregion
+
+        #region Members
+        private string imageFilePath = "";
+        private string imageFileName = "";
+        private Bitmap imageBMP = null;
+        #endregion
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+#if DEBUG
+            openFileDialogImage.InitialDirectory = Application.StartupPath.Replace("\\bin\\Debug", "") + "\\Images";
+#else
+            openFileDialogImage.InitialDirectory = Application.StartupPath;
+#endif
+            
+            if (!Directory.Exists(Application.StartupPath + "\\Temp"))
+                Directory.CreateDirectory(Application.StartupPath + "\\Temp");
+
+            if (!Directory.Exists(Application.StartupPath + "\\IamgeLetters"))
+                Directory.CreateDirectory(Application.StartupPath + "\\ImageLetters");
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialogImage.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    textBoxImage.Text = openFileDialogImage.FileName;
+                    imageFilePath = openFileDialogImage.FileName;
+                    imageFileName = openFileDialogImage.SafeFileName;
+                    imageBMP = new Bitmap(imageFilePath, true);
+                    pictureBoxCaptcha.Image = imageBMP;
+                    buttonSplit.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (imageBMP == null)
+                    return;
+
+                #region Stage 1 : Remove grid
+                if (File.Exists(Application.StartupPath + "\\Temp\\stage1.bmp"))
+                    File.Delete(Application.StartupPath + "\\Temp\\stage1.bmp");
+
+                if (File.Exists(Application.StartupPath + "\\Temp\\original.bmp"))
+                    File.Delete(Application.StartupPath + "\\Temp\\original.bmp");
+
+                imageBMP.Save(Application.StartupPath + "\\Temp\\original.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+
+                for (int i = 0; i < imageBMP.Width; i++)
+                    for (int j = 0; j < imageBMP.Height; j++)
+                    {
+                        if (imageBMP.GetPixel(i, j) == GRIDCOLOR) //remove grid
+                            imageBMP.SetPixel(i, j, Color.FromArgb(255, 255, 255, 255));
+                    }
+
+                imageBMP.Save(Application.StartupPath + "\\Temp\\stage1.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                pictureBoxCaptcha.Image = imageBMP;
+                #endregion
+
+                #region Stage 2 : Make white background
+                if (File.Exists(Application.StartupPath + "\\Temp\\stage2.bmp"))
+                    File.Delete(Application.StartupPath + "\\Temp\\stage2.bmp");
+
+                //change the lightest grey nuances to white
+                for (int i = 0; i < imageBMP.Width; i++)
+                    for (int j = 0; j < imageBMP.Height; j++)
+                    {
+                        if (imageBMP.GetPixel(i, j).B > Color.FromArgb(255, 108, 108, 108).B) //108, 108, 108
+                            imageBMP.SetPixel(i, j, Color.FromArgb(255, 255, 255, 255));
+                    }
+
+                imageBMP.Save(Application.StartupPath + "\\Temp\\stage2.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                pictureBoxCaptcha.Image = imageBMP;
+                #endregion
+
+                #region Stage 3 : Make letters black
+                if (File.Exists(Application.StartupPath + "\\Temp\\stage3.bmp"))
+                    File.Delete(Application.StartupPath + "\\Temp\\stage3.bmp");
+
+                Color b = imageBMP.GetPixel(0, 0);
+                for (int i = 0; i < imageBMP.Width; i++)
+                    for (int j = 0; j < imageBMP.Height; j++)
+                    {
+                        if (imageBMP.GetPixel(i, j) != Color.FromArgb(255,255,255,255)) 
+                            imageBMP.SetPixel(i, j, Color.FromArgb(255,0,0,0)); //black
+                    }
+
+                imageBMP.Save(Application.StartupPath + "\\Temp\\stage3.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                pictureBoxCaptcha.Image = imageBMP;
+                #endregion
+
+                MessageBox.Show("Succesfully finished", "Message", MessageBoxButtons.OK);
+                buttonSplit.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void buttonSplit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(Application.StartupPath + "\\Temp\\stage3.bmp"))
+                {
+                    MessageBox.Show("The image must be processed first", "Error");
+                    buttonSplit.Enabled = false;
+                    return;
+                }
+
+                imageBMP = new Bitmap(Application.StartupPath + "\\Temp\\stage3.bmp");
+                int lenLetters = imageBMP.Width / 3;
+                Bitmap letter1 = new Bitmap(lenLetters, imageBMP.Height);
+                Bitmap letter2 = new Bitmap(lenLetters, imageBMP.Height);
+                Bitmap letter3 = new Bitmap(lenLetters + imageBMP.Width % 3, imageBMP.Height);
+
+                #region Split image in 3 letters
+                for (int i = 0; i < letter1.Width; i++)
+                    for (int j = 0; j < imageBMP.Height; j++)
+                        letter1.SetPixel(i, j, imageBMP.GetPixel(i, j));
+
+                for (int i = 0; i < letter2.Width; i++)
+                    for (int j = 0; j < imageBMP.Height; j++)
+                        letter2.SetPixel(i, j, imageBMP.GetPixel(i + lenLetters, j));
+
+                for (int i = 0; i < letter3.Width; i++)
+                    for (int j = 0; j < imageBMP.Height; j++)
+                        letter3.SetPixel(i, j, imageBMP.GetPixel(i + 2 * lenLetters, j));
+                #endregion
+
+                #region Crop useless white regions
+                Point leftTop = getLeftMostBlackPoint(letter1);
+                Point rightBottom = getRightMostBlackPoint(letter1);
+                Bitmap letter1_trimed = new Bitmap(rightBottom.X - leftTop.X, rightBottom.Y - leftTop.Y);
+
+                for (int i = 0; i < letter1_trimed.Width; i++)
+                    for (int j = 0; j < letter1_trimed.Height; j++)
+                        letter1_trimed.SetPixel(i, j, letter1.GetPixel(i + leftTop.X, j + leftTop.Y));
+
+                leftTop = getLeftMostBlackPoint(letter2);
+                rightBottom = getRightMostBlackPoint(letter2);
+                Bitmap letter2_trimed = new Bitmap(rightBottom.X - leftTop.X, rightBottom.Y - leftTop.Y);
+
+                for (int i = 0; i < letter2_trimed.Width; i++)
+                    for (int j = 0; j < letter2_trimed.Height; j++)
+                        letter2_trimed.SetPixel(i, j, letter2.GetPixel(i + leftTop.X, j + leftTop.Y));
+
+                leftTop = getLeftMostBlackPoint(letter3);
+                rightBottom = getRightMostBlackPoint(letter3);
+                Bitmap letter3_trimed = new Bitmap(rightBottom.X - leftTop.X, rightBottom.Y - leftTop.Y);
+
+                for (int i = 0; i < letter3_trimed.Width; i++)
+                    for (int j = 0; j < letter3_trimed.Height; j++)
+                        letter3_trimed.SetPixel(i, j, letter3.GetPixel(i + leftTop.X, j + leftTop.Y));
+                #endregion
+
+                #region Save the letters to images
+                if (File.Exists(Application.StartupPath + "\\ImageLetters\\letter1.bmp"))
+                    File.Delete(Application.StartupPath + "\\ImageLetters\\letter1.bmp");
+
+                if (File.Exists(Application.StartupPath + "\\ImageLetters\\letter2.bmp"))
+                    File.Delete(Application.StartupPath + "\\ImageLetters\\letter2.bmp");
+
+                if (File.Exists(Application.StartupPath + "\\ImageLetters\\letter3.bmp"))
+                    File.Delete(Application.StartupPath + "\\ImageLetters\\letter3.bmp");
+
+                letter1_trimed.Save(Application.StartupPath + "\\ImageLetters\\letter1.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                letter2_trimed.Save(Application.StartupPath + "\\ImageLetters\\letter2.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                letter3_trimed.Save(Application.StartupPath + "\\ImageLetters\\letter3.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                #endregion
+
+                imageBMP.Dispose();
+                MessageBox.Show("Succesfully finished", "Message", MessageBoxButtons.OK);
+
+                #region Open folder location
+                string argument = "/select, " + Application.StartupPath + "\\ImageLetters\\letter1.bmp";
+                System.Diagnostics.Process.Start("explorer.exe", argument);
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private Point getLeftMostBlackPoint(Bitmap image)
+        {
+            int leftX = 0, leftY = 0;
+            bool finished = false;
+
+            //from top -> bottom
+            for (int x = 0; x < image.Width; x++)
+            {
+                if (finished)
+                    break;
+
+                for (int y = 0; y < image.Height; y++)
+                    if (image.GetPixel(x, y) == Color.FromArgb(255, 0, 0, 0))
+                    {
+                        leftX = x;
+                        finished = true;
+                        break;
+                    }
+            }
+
+            //from left -> right
+            finished = false;
+            for (int y = 0; y < image.Height; y++)
+            {
+                if (finished)
+                    break;
+
+                for (int x = 0; x < image.Width; x++)
+                    if (image.GetPixel(x, y) == Color.FromArgb(255, 0, 0, 0))
+                    {
+                        leftY = y;
+                        finished = true;
+                        break;
+                    }
+            }
+
+         /*   if (leftX > 0)
+                leftX--;
+            if (leftY > 0)
+                leftY--;
+            */
+            return new Point(leftX, leftY);
+        }
+
+        private Point getRightMostBlackPoint(Bitmap image)
+        {
+            int rightX = image.Width, rightY = image.Height;
+            bool finished = false;
+
+            //from bottom -> top
+            finished = false;
+            for (int x = image.Width - 1; x >= 0; x--)
+            {
+                if (finished)
+                    break;
+
+                for (int y = image.Height - 1; y >= 0; y--)
+                    if (image.GetPixel(x, y) == Color.FromArgb(255, 0, 0, 0))
+                    {
+                        rightX = x;
+                        finished = true;
+                        break;
+                    }
+            }
+
+            //from right -> left
+            finished = false;
+            for (int y = image.Height - 1; y >= 0; y--)
+            {
+                if (finished)
+                    break;
+
+                for (int x = image.Width - 1; x >= 0; x--)
+                    if (image.GetPixel(x, y) == Color.FromArgb(255, 0, 0, 0))
+                    {
+                        rightY = y;
+                        finished = true;
+                        break;
+                    }
+            }
+
+            if (rightX < image.Width - 1)
+                rightX++;
+            if (rightY < image.Height - 1)
+                rightY++;
+            
+            return new Point(rightX, rightY);
+        }
+    }
+}
